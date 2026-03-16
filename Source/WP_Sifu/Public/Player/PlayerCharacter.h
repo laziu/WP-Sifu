6bea4abfa84e0390df7959637e689da9b6b3e3a2
Geonu Kim <laziu.cc@gmail.com>
2026-03-16 19:43:56 +0900

    feat(player): basic player character

---
+++ Source/WP_Sifu/Public/Player/PlayerCharacter.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class WP_SIFU_API APlayerCharacter : public ACharacter, public IAbilitySystemInterface
+
+
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// AbilitySystemInterface
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Abilities)
+	TObjectPtr<class UAbilitySystemComponent> AbilitySystemComp;
+
+	UPROPERTY()
+	TObjectPtr<class UHealthAttributeSet> HealthAttribs;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Abilities)
+	float MaxHealth = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Abilities)
+	float MaxStructure = 100.f;
+};
