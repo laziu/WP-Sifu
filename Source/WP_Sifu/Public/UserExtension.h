f86952a035804a3f472cf1bfe0fcca279c95a56a
Geonu Kim <laziu.cc@gmail.com>
2026-03-19 17:20:32 +0900

    feat: extensions for open class, create subobject

--- Source/WP_Sifu/Public/UserExtension.h
+++ Source/WP_Sifu/Public/UserExtension.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <functional>
 #include "CoreMinimal.h"
-#include "UserExtension.generated.h"
 
 
 namespace Ext
 {
-	template <class T>
+	// --- ConstructorHelpers::FObjectFinder helper functions ---
+
+	template <typename T>
 	bool SetObject(TObjectPtr<T>& Target, const TCHAR* ObjectToFind)
 	{
 		if (ConstructorHelpers::FObjectFinder<T> Object(ObjectToFind); Object.Succeeded())
@@ -36,14 +36,59 @@ namespace Ext
 		return Object.Object;
 	}
 
-	template <class T>
-	bool WithObject(const TCHAR* ObjectToFind, std::function<void(TObjectPtr<T>&)> SuccessFunction)
+
+	// --- ConstructorHelpers::FClassFinder helper functions ---
+
+	template <typename T>
+	bool SetClass(TSubclassOf<T>& Target, const TCHAR* ClassToFind)
 	{
-		if (ConstructorHelpers::FObjectFinder<T> Object(ObjectToFind); Object.Succeeded())
+		if (ConstructorHelpers::FClassFinder<T> Class(ClassToFind); Class.Succeeded())
 		{
-			SuccessFunction(Object.Object);
+			Target = Class.Class;
 			return true;
 		}
 		return false;
 	}
+
+	template <class T>
+	TSubclassOf<T> OpenClass(const TCHAR* ClassToFind, const TSubclassOf<T>& FailureValue = nullptr)
+	{
+		if (ConstructorHelpers::FClassFinder<T> Class(ClassToFind); Class.Succeeded())
+		{
+			return Class.Class;
+		}
+		return FailureValue;
+	}
+
+	template <class T>
+	TSubclassOf<T> OpenClassChecked(const TCHAR* ClassToFind)
+	{
+		ConstructorHelpers::FClassFinder<T> Class(ClassToFind);
+		check(Class.Succeeded());
+		return Class.Class;
+	}
+
+
+	// Helper class for UObject
+	class Bind final
+	{
+		TObjectPtr<UObject> Owner;
+
+	public:
+		Bind(const TObjectPtr<UObject>& Owner) : Owner(Owner)
+		{
+		}
+
+		// CreateDefaultSubobject helper function
+		template <class T>
+		void CreateSubobject(TObjectPtr<T>& Target, FName ObjectName) const
+		{
+			Target = Owner->CreateDefaultSubobject<T>(ObjectName);
+		}
+	};
+}
+
+#define EXT_CREATE_SUBOBJECT(Target) \
+{ \
+	Ext::Bind(this).CreateSubobject(Target, TEXT(#Target)); \
 }
