57003d8eb69c703e62cc37a68259b311c6087f6a
Geonu Kim <laziu.cc@gmail.com>
2026-04-06 10:15:43 +0900

    feat(enemy): health

--- Source/WP_Sifu/Private/Enemybase.cpp
+++ Source/WP_Sifu/Private/Enemybase.cpp
@@ -3,6 +3,9 @@
 
 #include "Enemybase.h"
 
+#include "DummyCombatInteractionComponent.h"
+#include "HealthAttributeSet.h"
+#include "UserExtension.h"
 #include "Components/BoxComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
@@ -10,13 +13,13 @@
 // Sets default values
 AEnemybase::AEnemybase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
+
 	collisionComp = CreateDefaultSubobject<UBoxComponent>("collisionComp");
 	collisionComp->SetupAttachment(RootComponent);
-	collisionComp->SetBoxExtent(FVector(32.f,32.f,88.f));
-	
+	collisionComp->SetBoxExtent(FVector(32.f, 32.f, 88.f));
+
 	// 이동 방향으로 자동 회전
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.f, 480.f, 0.f);
@@ -25,28 +28,47 @@ AEnemybase::AEnemybase()
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationRoll = false;
-	
+
 	//GetMesh()->SetCollisionProfileName("")
+
+	// Ability system
+	EXT_CREATE_DEFAULT_SUBOBJECT(AbilitySystem);
+	EXT_CREATE_DEFAULT_SUBOBJECT(HealthAttributes);
+	EXT_CREATE_DEFAULT_SUBOBJECT(CombatInteractionComp);
+
+	// Initialize attributes
+	HealthAttributes->InitHealth(MaxHealth);
+	HealthAttributes->InitMaxHealth(MaxHealth);
+	HealthAttributes->InitStructure(0.f);
+	HealthAttributes->InitMaxStructure(MaxStructure);
 }
 
 // Called when the game starts or when spawned
 void AEnemybase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	AbilitySystem->InitAbilityActorInfo(this, this);
 }
 
 // Called every frame
 void AEnemybase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void AEnemybase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
+UAbilitySystemComponent* AEnemybase::GetAbilitySystemComponent() const
+{
+	return AbilitySystem;
+}
+
+class UCombatInteractionComponentBase* AEnemybase::GetCombatInteractionComponent() const
+{
+	return CombatInteractionComp;
+}
